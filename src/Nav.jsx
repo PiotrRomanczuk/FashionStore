1ff7c9fbcae2590d15fcbcc14eadad8d352cf33d
K0st3k6969 <p.romanczuk@gmail.com>
2022-06-19 20:35:20 +0200

    Adding NAv

---
+++ src/Nav.jsx
@@ -0,0 +1,17 @@
+import './styles.css'
+
+const Nav = () => {
+  return <nav>
+    <div class="logo">
+        <h4>The Nav</h4>
+    </div>
+    <ul>
+        <li><a href="#">Home<a/></li>
+        <li><a href="#">About<a/></li>
+        <li><a href="#">Work<a/></li>
+        <li><a href="#">Contact<a/></li>
+    </ul>
+  </nav>
+};
+
+export default Nav;
